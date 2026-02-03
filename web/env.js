b17481ce31894d899c251c6e0f605df1696bb0f8
Jesus <124712591+jesusramirez99@users.noreply.github.com>
2026-02-03 13:15:25 -0600

    UPDATE env.js

---
+++ web/env.js
@@ -0,0 +1,8 @@
+window.env = {
+  //Server OHS
+  BASE_URL: "http://10.10.29.18/TrenSeguroDev/api",
+  //Server 150(Ambiente PROD)
+  BASE_URL1: "http://10.10.76.150/TrenSeguroDev/api",
+  //Server 150(Ambiente DEV)
+  BASE_URL3: "http://10.10.76.150/TrenSeguro/api",
+};
